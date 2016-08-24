d5e440fb515b15451b6e1a723e03cd31f83b3dd3
阿福体育科技公司 <afufitness@afutiyukejigongsideMacBook-Pro-2.local>
2016-08-24 16:43:10 +0800

    project 初始化

---
+++ huoyiProhectBeata1_0/huoyiProhectBeata1_0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  huoyiProhectBeata1_0
+//
+//  Created by 阿福体育科技公司 on 16/8/24.
+//  Copyright © 2016年 阿福体育科技公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ huoyiProhectBeata1_0/huoyiProhectBeata1_0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  huoyiProhectBeata1_0
+//
+//  Created by 阿福体育科技公司 on 16/8/24.
+//  Copyright © 2016年 阿福体育科技公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
